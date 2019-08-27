55fc65bc2fb225e37db9d4c01ddbe8cac9942eaa
Alex Gonzales <acgonzales42@gmail.com>
2019-08-26 21:39:30 -0600

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+// Alexander Gonzales
+// CS 371 - Software Dev
+// helloworld.java
+// 8/26/2019
+// Sample java file to practice with Github
+
+public class helloworld {
+   public static void main(String[] args) {
+      System.out.println("Hello, world!");
+   }// end main
+}// end helloworld
